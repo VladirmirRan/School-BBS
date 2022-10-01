4dcf887c37f8007e31ed47eee405892e969bc879
Nier <1592654136@qq.com>
2022-10-01 19:05:35 +0800

    commit:first commit

---
+++ src/main/java/com/school/bbs/SchoolBbsApplication.java
@@ -0,0 +1,13 @@
+package com.school.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolBbsApplication.class, args);
+    }
+
+}
