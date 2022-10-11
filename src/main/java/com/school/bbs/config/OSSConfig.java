c50c7504432c1bfc2d7173588efae524f99ffcdd
Nier_2b <1592654136@qq.com>
2022-10-11 16:47:57 +0800

    feat:OSS改为本地

--- src/main/java/com/school/bbs/config/OSSConfig.java
+++ src/main/java/com/school/bbs/config/OSSConfig.java
@@ -2,6 +2,7 @@ package com.school.bbs.config;
 
 import com.school.bbs.service.StorageService;
 import com.school.bbs.service.impl.OSSCloudStorage;
+import com.school.bbs.service.impl.OSSLocalStorage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
@@ -23,6 +24,18 @@ public class OSSConfig {
         public StorageService ossCloudStorage(final OSSFileProperties fileProperties,
                                               final OSSCloudProperties cloudProperties) {
             return new OSSCloudStorage(fileProperties, cloudProperties);
+        }
+    }
+
+    @Configuration
+    @ConditionalOnMissingBean(EmbeddedMongoProperties.Storage.class)
+    @ConditionalOnProperty(prefix = "oss.file", name = "type", havingValue = "LOCAL")
+    public static class OSSLOCALConfiguration {
+
+        @Bean
+        public StorageService ossCloudStorage(final OSSFileProperties fileProperties,
+                                              final OSSCloudProperties cloudProperties) {
+            return new OSSLocalStorage(fileProperties);
         }
     }
 }
